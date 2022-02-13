eee81dbec34f51f594ccf29c2acb13bf59882f49
celebrater <tkgm45nbchk@gmail.com>
2022-02-13 15:53:57 +0900

    click test different execution that depends type of button succeeded

--- src/components/HiddenButton.js
+++ src/components/HiddenButton.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-const clickButton = () => {
-    console.log("clicked");
-}
-
 export const HiddenButton = (props) => {
-    const buttonType = props.type;
+    // const buttonType = props.type;
     const style = {
         opacity: 0.2,
         backgroundColor: "white",
@@ -18,9 +14,23 @@ export const HiddenButton = (props) => {
         left: props.left === undefined ? "auto" : props.left,
         bottom: props.bottom === undefined ? "auto" : props.bottom
     }
-    console.log(style);
-    console.log(props.right);
-    console.log(buttonType);
+
+    const clickButton = () => {
+        // console.log("clicked");
+        let msg = "clicked ";
+        switch (props.type){
+            case "top": msg += "top"; break;
+            case "right": msg += "right"; break;
+            case "bottom": msg += "bottom"; break;
+            case "left": msg += "left"; break;
+            default: msg += "unknown"; break;
+            // default: msg += "unknown";
+        }
+        // console.log(msg);
+    }
+    // console.log(style);
+    // console.log(props.right);
+    // console.log(buttonType);
 
     return (
         <button style={style} onClick={clickButton}>テスト</button>
